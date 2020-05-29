f568fe61f90680dad204bc41af0245df553b8507
V <maja5225@hotmail.com>
2020-05-30 01:53:00 +0200

    media query

--- script/team.js
+++ script/team.js
@@ -1,55 +1,58 @@
-
-
 var teamMembers = [
     {
         name:"Fran",
         position: "CEO",
         email:"fran@index.com",
-        image:`src="media/person1.jpg"`
+        image:"media/person1.jpg",
+        tel: "0680 &#47; 12 04 569", 
     },
     {
         name:"Bill",
         position: "General Manager",
         email:"bill@index.com",
-        image:`src="media/person2.jpg"`
+        image:"media/person2.jpg",
+        tel: "0680 &#47; 12 34 507", 
     },
     {
         name:"Fritz",
         position: "HR Manager",
         email:"fritz@index.com",
-        image:`src="media/person3.jpg"`
+        image:"media/person3.jpg",
+        tel: "0681 &#47; 10 34 567", 
     },
     {
         name:"Bella",
         position: "Acounting",
         email: "bella@index.com",
-        image:`src="media/person4.jpg"`
+        image:"media/person4.jpg",
+        tel: "0680 &#47; 12 94 507", 
     },
     {
         name:"Max",
         position: "Senior Fetch Expert",
         email:"max@index.com",
-        image:`src="media/person5.jpg"`
+        image:"media/person5.jpg",
+        tel: "0681 &#47; 92 34 567", 
     },
     {
         name:"Kenny",
         position: "Junior Fetch Expert",
         email:"kenny@index.com",
-        image:`src="media/person6.jpg"`
+        image:"media/person6.jpg",
+        tel: "0680 &#47; 12 04 967", 
     },
 ];
 
-var i;
-for (i = 0; i < teamMembers.length; i++) {
+for (var i = 0; i < teamMembers.length; i++) {
     document.getElementById("team").innerHTML += `
         <div class="person">
-            <img ${teamMembers[i].image} alt="Team Member">
+            <img src="${teamMembers[i].image}" alt="Team Member">
             <p>${teamMembers[i].name}</p>
             <div class="overlay">
                 <div id="info" class="overlay_text">
                     <p>${teamMembers[i].position}</p>
                     <p>${teamMembers[i].email}</p>
-                    <p></p>
+                    <p class="small">tel: ${teamMembers[i].tel}</p>
                 </div>
             </div>
         </div>
